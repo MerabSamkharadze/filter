ee8557f971986bb327f598fee1afa363f8aafaf2
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-19 00:02:38 +0400

    add filter fnc and kayUp event on search input

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 const members = document.getElementById("members");
+const search = document.getElementById("search");
+const membersArr = [];
+let li;
 const asyncCall = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
@@ -7,8 +10,12 @@ const asyncCall = async () => {
     const dataResponse = await response.json();
 
     createLists(dataResponse);
+    search.addEventListener("keyup", function () {
+      filterItem(this.value);
+    });
   } catch (error) {
     const errorText = document.createElement("h1");
+    console.log(error);
     errorText.innerText = "server problem";
     document.body.appendChild(errorText);
   }
@@ -17,12 +24,20 @@ const asyncCall = async () => {
 asyncCall();
 
 function createLists(data) {
-  console.log(data);
-  let fragment = new DocumentFragment();
   data.forEach((element) => {
-    const li = document.createElement("li");
+    li = document.createElement("li");
+    li.classList.add("li_members");
     li.textContent = element.title;
-    fragment.appendChild(li);
+    membersArr.push(li);
+    members.appendChild(li);
   });
-  members.appendChild(fragment);
+}
+function filterItem(value) {
+  membersArr.forEach((element) => {
+    if (element.innerText.includes(value)) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
 }
