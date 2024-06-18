5134ecd4adab161a66cb958ce9bd2de02ccc9aa0
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-18 23:14:37 +0400

    create li elements

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
+const members = document.getElementById("members");
 const asyncCall = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     if (!response.ok) throw new Error("can not fetch data");
     const dataResponse = await response.json();
-    console.log(dataResponse);
-    return dataResponse;
+
+    createLists(dataResponse);
   } catch (error) {
     const errorText = document.createElement("h1");
     errorText.innerText = "server problem";
@@ -14,3 +15,14 @@ const asyncCall = async () => {
 };
 
 asyncCall();
+
+function createLists(data) {
+  console.log(data);
+  let fragment = new DocumentFragment();
+  data.forEach((element) => {
+    const li = document.createElement("li");
+    li.textContent = element.title;
+    fragment.appendChild(li);
+  });
+  members.appendChild(fragment);
+}
